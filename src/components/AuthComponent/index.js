e85bb728473780d15826f8ad29c07c8b2116cb20
Ian Ibbotson <ianibbo@gmail.com>
2017-11-13 14:30:36 +0000

    added more experiments

--- src/components/AuthComponent/index.js
+++ src/components/AuthComponent/index.js
@@ -16,31 +16,13 @@ class AuthComponent extends Component {
 
 }
 
-export const fakeAuth = {
-  isAuthenticated: false,
-  wibble:"this is a string",
-  authenticate(cb) {
-    this.isAuthenticated = true
-    setTimeout(cb, 100) // fake async
-  },
-  signout(cb) {
-    this.isAuthenticated = false
-    setTimeout(cb, 100)
-  },
-  user_data: {
-    name:null,
-    email:null,
-    token:null
-  }
-}
+export const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
 
-  export const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route {...rest} render={props => (
-
-      rest.user.isAuthenticated ? (
+  rest.user.isAuthenticated ? (
         // User is authenticated -- send directly to component
         <Component {...props}/>
-      ) : window.location.href='https://apps.semweb.co/auth/prompt?flow=http://localhost:3000/callback'
+  ) : window.location.href='https://apps.semweb.co/auth/prompt?flow=http://localhost:3000/callback'
       // (
         // User is not authenticated -- redirect to auth service
         // <Redirect to={{
@@ -49,8 +31,8 @@ export const fakeAuth = {
         // }}/>
       // window.location.href='https://apps.semweb.co/auth/prompt?flow=http://localhost:3000/callback';
       // )
-    )}/>
-  )
+  )}/>
+)
 
 
 export default AuthComponent;
--- src/components/CallbackComponent/index.js
+++ src/components/CallbackComponent/index.js
@@ -4,9 +4,17 @@ import {render} from 'react-dom'
 // Extends React.Compoent
 class CallbackComponent extends Component {
 
+  // this.props.location.query
+
   render(){
     return <div>
-      this is for callbacks
+      this is for callbacks 
+      <ul>
+        <li>{this.props.location ? this.props.location.query : 'no this.props.location.query'}</li>
+        <li>{this.props.to ? this.props.to.query : 'no this.props.to.query'}</li>
+        <li>{window.location ? window.location.hash : 'No windown.location.hash'}</li>
+        <li>{window.location ? window.location : 'No windown.location'}</li>
+      </ul>
     </div>;
   }
 
